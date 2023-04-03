eff421f794475f90940ee0db1de3a9ed72d0a463
Francesco Pirrone <francesco.pirrone@finwave.it>
2023-04-03 22:34:24 +0200

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GamesComponent } from './games/games.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDatepickerModule } from '@angular/material/datepicker'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatTableModule,
+    MatButtonModule,
+
+    MatDatepickerModule,
+    MatNativeDateModule,
+
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+
+    HttpClientModule,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/games/games.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common'
+import { MatTableDataSource } from '@angular/material/table';
+export interface GameElement {
+  name: string;
+  description:string;
+  /**
+   * Data in formato dd/MM/yyyy
+   */
+  dataPubblicazione:string;
+  price:number;
+
+}
+
+const ELEMENT_DATA: GameElement[] = [
+  // che dati ?? 
+];
+
+@Component({
+  selector: 'app-games',
+  templateUrl: './games.component.html',
+  styleUrls: ['./games.component.css']
+})
+export class GamesComponent {
+  constructor(private datePipe:DatePipe){}
+  displayedColumns: string[] = ['name', 'description', 'price'];
+  dataSource =  new MatTableDataSource(ELEMENT_DATA);
+  dataSelezionata:Date|null=null;
+
+  applyFilter() {
+    const filterValue = this.datePipe.transform(this.dataSelezionata,"dd/MM/yyyy");
+    // filtrare su una determinata colonna
+  }
+}
